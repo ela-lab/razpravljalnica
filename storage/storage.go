5823bbae369f5fed76bbe55ef3740afa41baeef8
ela-lab <er38622@student.uni-lj.si>
2026-01-02 15:26:35 +0100

    Fix UpdateMessage function

--- storage/storage.go
+++ storage/storage.go
@@ -18,7 +18,7 @@ type TopicStorage struct {
 
 type SubscriptionStorage struct {
 	dict map[int64][]int64 //user_id -> seznam topic_id
-    lock sync.RWMutex
+	lock sync.RWMutex
 }
 
 type MessageStorage struct {
@@ -37,7 +37,7 @@ SUBSCRIPTION - Create
 TOPIC - Create, Read(vse teme)
 */
 
-//USERS
+// USERS
 func (us *UserStorage) CreateUser(user razpravljalnica.User, ret *struct{}) error {
 	us.lock.Lock()
 	defer us.lock.Unlock() //v vsakem primeru se izvede unlock
@@ -47,19 +47,19 @@ func (us *UserStorage) CreateUser(user razpravljalnica.User, ret *struct{}) erro
 
 func (us *UserStorage) ReadUser(id int64, name *string) error {
 	us.lock.RLock()
-	defer us.lock.RUnlock() 
+	defer us.lock.RUnlock()
 	*name = us.dict[id].Name
 	return nil
 }
 
-//LIKES
+// LIKES
 func (ls *LikeStorage) CreateLike(like razpravljalnica.Like, ret *struct{}) error {
 	ls.lock.Lock()
 	defer ls.lock.Unlock()
 	_, keyExists := ls.dict[like.MessageId]
 	if keyExists == false {
-    //še ni nobenega like-a za to sporočilo -> ustvarimo prazen slice
-    ls.dict[like.MessageId] = make([]int64, 0)
+		//še ni nobenega like-a za to sporočilo -> ustvarimo prazen slice
+		ls.dict[like.MessageId] = make([]int64, 0)
 	}
 
 	//preveri, če like za user-ja že obstaja
@@ -73,7 +73,7 @@ func (ls *LikeStorage) CreateLike(like razpravljalnica.Like, ret *struct{}) erro
 	return nil
 }
 
-//št. like-ov na message
+// št. like-ov na message
 func (ls *LikeStorage) ReadLikes(messageId int64, likes *int64) error {
 	ls.lock.RLock()
 	defer ls.lock.RUnlock()
@@ -86,61 +86,62 @@ func (ls *LikeStorage) ReadLikes(messageId int64, likes *int64) error {
 	return nil
 }
 
-//MESSAGES
+// MESSAGES
 func (ms *MessageStorage) CreateMessage(message razpravljalnica.Message, ret *struct{}) error {
 	ms.lock.Lock()
 	defer ms.lock.Unlock()
 	//če še message v topic ne obstaja
 	if _, ok := ms.dict[message.TopicId]; !ok {
-        ms.dict[message.TopicId] = []razpravljalnica.Message{}
-    }
-    ms.dict[message.TopicId] = append(ms.dict[message.TopicId], message)
-    return nil
+		ms.dict[message.TopicId] = []razpravljalnica.Message{}
+	}
+	ms.dict[message.TopicId] = append(ms.dict[message.TopicId], message)
+	return nil
 }
 
-func(ms *MessageStorage) DeleteMessage(messageId, topicId int64, ret *struct{}) error {
+func (ms *MessageStorage) DeleteMessage(messageId, topicId int64, ret *struct{}) error {
 	ms.lock.Lock()
 	msgs, ok := ms.dict[topicId]
-    if !ok {
-        return nil //tema ne obstaja
-    }
+	if !ok {
+		return nil //tema ne obstaja
+	}
 
 	//nov slice za filtriranje sporočil
-    newMsgs := []razpravljalnica.Message{}
-    for _, m := range msgs {
-        if m.Id != messageId {
-            newMsgs = append(newMsgs, m)
-        }
-    }
+	newMsgs := []razpravljalnica.Message{}
+	for _, m := range msgs {
+		if m.Id != messageId {
+			newMsgs = append(newMsgs, m)
+		}
+	}
 
-    ms.dict[topicId] = newMsgs
-    return nil
+	ms.dict[topicId] = newMsgs
+	return nil
 }
 
 func (ms *MessageStorage) ReadMessages(topicId int64, dict *[]razpravljalnica.Message) error {
 	ms.lock.RLock()
-    defer ms.lock.RUnlock()
+	defer ms.lock.RUnlock()
 
-    msgs, ok := ms.dict[topicId]
-    if !ok {
+	msgs, ok := ms.dict[topicId]
+	if !ok {
 		*dict = []razpravljalnica.Message{}
 		return nil
-    }
+	}
 
-    *dict = make([]razpravljalnica.Message, len(msgs))
-    copy(*dict, msgs)
+	*dict = make([]razpravljalnica.Message, len(msgs))
+	copy(*dict, msgs)
 
-    return nil
+	return nil
 }
 
 func (ms *MessageStorage) UpdateMessage(message razpravljalnica.Message, ret *struct{}) error {
 	ms.lock.Lock()
 	defer ms.lock.Unlock()
-	if msgs,ok := ms.dict[message.TopicId]; ok {
+	if msgs, ok := ms.dict[message.TopicId]; ok {
 		for i, j := range msgs {
-			if j.Id == message.Id
-			ms.dict[message.TopicId][i] = message
-			return nil
+			if j.Id == message.Id {
+				ms.dict[message.TopicId][i] = message
+				return nil
+			}
 		}
 	}
 	return nil
